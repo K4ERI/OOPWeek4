efa029f1b2d0226b94a6b744ee98d9c348589d35
K4ERI <m.khazq@gmail.com>
2024-09-25 09:25:16 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("How many transaction today?");
+        string numStr = Console.ReadLine();
+        int num = Int32.Parse(numStr); //cast from string to integer
+
+
+        TransactionList transactionList = new TransactionList();
+        transactionList.init(num);
+        for (int i = 0; i < num; i++)
+        {
+            // creating an object (variable of a certain class type)
+            Transaction transaction;
+            transaction = new Transaction();
+
+            transaction.setDate(new DateTime(2008, 6, 1));
+            transaction.setVal(-10.5f);
+            transactionList.Add(transaction);
+
+            Console.WriteLine("transaction value is " + transaction.getVal() + " at time " + transaction.getDate());
+        }
+        transactionList.Display();
+    }
+
+}
---
+++ TransactionList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class TransactionList
+{
+    //data
+    private Transaction[] transactions; // transaction is an array object
+    private int numTransaction;
+
+    //operation
+    public void init(int num) { transactions = new Transaction[num]; }
+    public void Add(Transaction transaction)
+    {
+        transactions[numTransaction] = transaction;
+    }
+    public void Display()
+    {
+        for (int i = 0; i < numTransaction; i++)
+        {
+            Console.WriteLine(transactions[i].ToString());
+        }
+    }
+}
